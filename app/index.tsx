801ec326ea5725ec76eb3a76ba00281ba2f2623a
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2026-02-19 22:34:16 +0200

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,67 @@
-import { Text, View } from "react-native";
+import {Text, View, StyleSheet, TouchableOpacity, Button, Alert} from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    const handleHello = () => {
+        // Alert.alert("Hello!");
+        Alert.alert('Hello!', 'lorem ipsum', [
+            {
+                text: 'Cancel',
+                onPress: () => console.log('Cancel Pressed'),
+                style: 'cancel',
+            },
+            {text: 'OK', onPress: () => console.log('OK Pressed')},
+        ]);
+    }
+
+    const handlePapa = () => {
+        Alert.alert("Papa!");
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Hello World!</Text>
+
+            <TouchableOpacity style={styles.buttonHello} onPress={handleHello}>
+                <Text style={styles.buttonHelloText}>Say Hello!</Text>
+            </TouchableOpacity>
+
+            <Button
+                onPress={handlePapa}
+                color="#841584"
+                title={"Say Papa!"}
+            />
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "center",
+        backgroundColor: "#906262",
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "#FFF",
+        marginBottom: 10
+    },
+    buttonHello: {
+        fontSize: 20,
+        fontWeight: "bold",
+        paddingHorizontal: 10,
+        paddingVertical: 10,
+        backgroundColor: "#1672a2",
+        borderRadius: 10,
+    },
+    buttonHelloText: {
+        color: "#FFF",
+    },
+    buttonPapa: {
+        paddingHorizontal: 10,
+        paddingVertical: 10,
+        backgroundColor: "#FFF",
+    }
+
+})
